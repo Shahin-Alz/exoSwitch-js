60db510fd55d070c9a89e25bb16225389d2e95fa
Shahin-Alz <shaalemzadeh@gmail.com>
2022-12-20 16:52:34 +0100

    ajout

---
+++ public/js/main.js
@@ -0,0 +1,146 @@
+// N1
+
+// let jour = prompt("jour de la semaine")
+
+// switch (jour) {
+//     case "lundi":
+//         console.log("On est lundi. Tu as cours  à MolenGeek et tu as sport en soirée.");
+//         break;
+
+// }
+
+// N2
+
+// let temps = prompt("meteo?")
+
+// switch (temps) {
+//     case "Il pleut":
+//         console.log("Je prend un parapluie");
+//         break;
+
+// }
+
+// N3 pas fini
+
+// let valeur = prompt("valeur de l'utilisateur")
+
+// switch (valeur) {
+//     case valeur % 2:
+//         alert("votre nombre est multiple de 2")
+//         break;
+
+//     default:
+//         alert("votre nombre multiple de 2")
+//         break;
+// }
+
+
+// N4 
+
+// let chiffre1 = +prompt("un premier chiffre")
+// let calcul = prompt("un signe")
+// let chiffre2 = +prompt("un deuxiemme chiffres")
+
+// switch (calcul) {
+//     case "+":
+//         console.log(chiffre1 + chiffre2);
+//         break;
+
+//     case "-":
+//         console.log(chiffre1 - chiffre2);
+//         break;
+
+//     case "*":
+//         console.log(chiffre1 * chiffre2);
+//         break;
+
+//     case "/":
+//         console.log(chiffre1 / chiffre2);
+//         break;
+
+
+// }
+
+// BONUS
+
+// let jour = +prompt("jour de la semaine")
+
+// switch (jour) {
+//     case 1:
+//         console.log("Lundi");
+//         break;
+
+//     case 2:
+//         console.log("Mardi");
+//         break;
+
+//     case 3:
+//         console.log("Mercredi");
+//         break;
+
+//     case 4:
+//         console.log("Jeudi");
+//         break;
+
+//     case 5:
+//         console.log("Vendredi");
+//         break;
+
+//     case 6:
+//         console.log("Samedi");
+//         break;
+
+//     case 7:
+//         console.log("Dimanche");
+//         break;
+
+// }
+
+// n2
+
+// let mois = +prompt("jour de la semaine")
+
+// switch (mois) {
+//     case 1:
+//         console.log("Janvier");
+//         break;
+
+//     case 2:
+//         console.log("Fevrier");
+//         break;
+
+//     case 3:
+//         console.log("Mars");
+//         break;
+
+//     case 4:
+//         console.log("Avril");
+//         break;
+
+//     case 5:
+//         console.log("Mai");
+//         break;
+
+//     case 6:
+//         console.log("Juin");
+//         break;
+
+//     case 7:
+//         console.log("Juillet");
+//         break;
+
+// }
+
+// n3
+
+// let nombre = +prompt("un chiffre entre 1 et 100")
+
+// switch (true) {
+//     case nombre >=90 && nombre<=100 :
+//         console.log("a");
+        
+//         break;
+
+//     default:
+//         break;
+// }
